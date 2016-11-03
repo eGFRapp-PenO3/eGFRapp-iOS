1757223ea8b447392cd5b5ebf9dc78928c0d9654
Elias Storme <elias.storme@gmail.com>
2016-11-03 17:52:48 +0100

    Initial Commit

---
+++ eGFR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eGFR
+//
+//  Created by Elias Storme on 03/11/16.
+//  Copyright © 2016 Elias Storme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
